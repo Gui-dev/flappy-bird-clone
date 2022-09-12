c24733e6dd9fa158fb664df2ddf43d9d5735533d
Gui-dev <adrianon10@gmail.com>
2022-09-12 19:40:57 -0300

    feat: add StaticBody2D collision

--- scripts/management/pipe.gd
+++ scripts/management/pipe.gd
@@ -2,8 +2,8 @@ extends Node2D
 class_name Pipe
 
 
-onready var top_pipe: Area2D = $top_pipe
-onready var bottom_pipe: Area2D = $bottom_pipe
+onready var top_pipe: StaticBody2D = $top_pipe
+onready var bottom_pipe: StaticBody2D = $bottom_pipe
 onready var score_area: Area2D = $score_area
 export(int) var walk_speed
 export(Array, String) var pipe_texture
@@ -27,9 +27,9 @@ func game_over() -> void:
 
 
 func _on_notifier_screen_exited() -> void:
-  queue_free()
+  queue_free() 
 
 
-func _on_score_area_body_entered(body: Bird) -> void:
+func _on_score_area_body_exited(body: Bird) -> void:
   if body is Bird and body.global_position.x > score_area.global_position.x:
     get_tree().call_group('interface', 'update_score')
