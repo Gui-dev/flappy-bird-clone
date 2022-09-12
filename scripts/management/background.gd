4cc6b8ef57a77fbca8c95d8fd10de39b4d0c3c2a
Gui-dev <adrianon10@gmail.com>
2022-09-12 19:41:50 -0300

    feat: add helper function game_over

--- scripts/management/background.gd
+++ scripts/management/background.gd
@@ -17,3 +17,7 @@ func _physics_process(delta: float) -> void:
   for children in get_children():
     if children is ParallaxLayer:
       children.motion_offset.x -= scenario_speed * delta
+
+
+func game_over() -> void:
+  set_physics_process(false)
