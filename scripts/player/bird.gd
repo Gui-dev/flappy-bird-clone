e9c1785d0c0abb6f04c9aef83e0f119763fba2c8
Gui-dev <adrianon10@gmail.com>
2022-09-13 02:59:32 -0300

    feat: add helper function spawn_sfx

--- scripts/player/bird.gd
+++ scripts/player/bird.gd
@@ -10,6 +10,7 @@ onready var animation: AnimationPlayer = $animation
 export(Array, String) var texture_list
 export(int) var gravity_speed
 export(int) var jump_speed
+export(PackedScene) var sfx_scene
 
 
 func _ready() -> void:
@@ -25,6 +26,7 @@ func _physics_process(delta: float) -> void:
     
   if Input.is_action_just_pressed('ui_select') and can_interact:
     velocity.y = jump_speed
+    spawn_sfx('res://assets/sfx/swoosh.ogg')
     
   velocity = move_and_slide(velocity)
 
@@ -38,11 +40,18 @@ func set_texture() -> void:
   randomize()
   var random_number: int = randi() % texture_list.size()
   bird_texture.texture = load(texture_list[random_number])
+  
+
+func spawn_sfx(effect: String) -> void:
+  var sfx: SoundEffect = sfx_scene.instance()
+  sfx.stream = load(effect)
+  get_tree().root.call_deferred('add_child', sfx)
 
 
 func _on_detection_area_body_entered(body: Object) -> void:
   if body.name != 'Bird':
     velocity.x = 0
     can_interact = false
+    spawn_sfx('res://assets/sfx/hit.ogg')
     animation.play('RESET')
     emit_signal('game_over') 
